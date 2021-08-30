b8adaf6a41f24590c5e51c457665a53d8009528d
Philimon Derib <philimonderib2019@gmail.com>
2021-08-30 13:57:11 +0300

    ident

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,15 +10,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i = 0;
+	size_t i = 0;
 
-        for (i = 0; i < size; i++)
-        {
-                if (array[i] == value) {
-                        printf("Value checked array[%lu] = [%d]\n",i,array[i]);
-                        return (i);
-                }
-                printf("Value checked array[%lu] = [%d]\n",i,array[i]);
-        }
-        return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value) {
+			printf("Value checked array[%lu] = [%d]\n",i,array[i]);
+			return (i);
+		}
+		printf("Value checked array[%lu] = [%d]\n",i,array[i]);
+	}
+	return (-1);
 }
