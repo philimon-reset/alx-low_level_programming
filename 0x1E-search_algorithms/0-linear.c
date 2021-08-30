cf68328a59f4db38af3f8d010a998b64fc264859
Philimon Derib <philimonderib2019@gmail.com>
2021-08-30 13:52:50 +0300

    linear and binary

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,24 @@
 #include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: a pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: int
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+        size_t i = 0;
+
+        for (i = 0; i < size; i++)
+        {
+                if (array[i] == value) {
+                        printf("Value checked array[%lu] = [%d]\n",i,array[i]);
+                        return (i);
+                }
+                printf("Value checked array[%lu] = [%d]\n",i,array[i]);
+        }
+        return (-1);
+}
