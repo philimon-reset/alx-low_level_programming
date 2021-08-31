3b18672c1888d9e0d42554907054241cd7af67d4
Philimon Derib <philimonderib2019@gmail.com>
2021-08-31 21:24:58 +0300

    prob

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,1 +1,55 @@
 #include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in an array of integers
+ * @array: a pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: int
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t i, mid, low = 0, high = size - 1;
+	int *temp = array;
+
+	if (size % 2 == 0)
+		mid = (size / 2) - 1;
+	else
+		mid = size / 2;
+	if (value == array[mid])
+	{
+		printf("Value checked array[%lu] = [%d]", mid, array[mid]);
+		return (mid);
+	}
+	while (mid < size && low != high)
+	{
+		printf("Value checked array[%lu] = [%d]", mid, array[mid]);
+		mid = probe(low, high, value, array);
+		if (value == array[mid])
+			return (mid);
+		else if (value > array[mid])
+			low = mid - 1;
+		else if (value < array[mid])
+			high = mid - 1;
+	}
+	printf("Value checked array[%lu] is out of range", mid);
+	return (-1);
+}
+
+/**
+ * probe - probe position
+ * @l: lowest index
+ * @h: highest index
+ * @v: value to be found
+ * @a: array to be used
+ * Return: prob value
+ */
+
+size_t probe(size_t l, size_t h, int v, int *a)
+{
+	size_t prob;
+
+	prob = l + (((double)(h - l) / (a[h] - a[l])) * (v - a[l]));
+	return (prob);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,6 +11,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+size_t probe(size_t l, size_t h, int v, int *a);
 
 /**
  * struct listint_s - singly linked list
@@ -24,11 +25,10 @@ int advanced_binary(int *array, size_t size, int value);
  */
 typedef struct listint_s
 {
-int n;
-size_t index;
-struct listint_s *next;
+        int n;
+        size_t index;
+        struct listint_s *next;
 } listint_t;
-
 
 listint_t *jump_list(listint_t *list, size_t size, int value);
 
@@ -45,10 +45,10 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
  */
 typedef struct skiplist_s
 {
-int n;
-size_t index;
-struct skiplist_s *next;
-struct skiplist_s *express;
+        int n;
+        size_t index;
+        struct skiplist_s *next;
+        struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
