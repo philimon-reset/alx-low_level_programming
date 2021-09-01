0c8e28a7f7530f20f7a3979451e363eb46ad5c9f
Philimon Derib <philimonderib2019@gmail.com>
2021-09-01 12:56:40 +0300

    pls work exp

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -10,7 +10,7 @@
 
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t i = 1, mid;
+	size_t i = 1, mid = 0;
 
 	if (array == NULL)
 		return (-1);
@@ -36,8 +36,6 @@ int exponential_search(int *array, size_t size, int value)
 	}
 	else
 		mid = binary(array, size - (i / 2), value);
-	if ((int)mid == -1)
-		return (-1);
 	return (mid + i / 2);
 }
 
--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -10,11 +10,10 @@
 int main(void)
 {
     int array[] = {
-        0, 1, 2, 3, 4, 7, 12, 15, 18, 19, 23, 54, 61, 62, 76, 99};
+        1, 0, 2, 3, 5};
     size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Found %d at index: %d\n\n", 62, exponential_search(array, size, 62));
-    printf("Found %d at index: %d\n\n", 3, exponential_search(array, size, 3));
-    printf("Found %d at index: %d\n", 999, exponential_search(array, size, 999));
+    int num = 1;
+    printf("Found %d at index: %d\n\n", num, exponential_search(array, size, num));
     return (EXIT_SUCCESS);
 }
