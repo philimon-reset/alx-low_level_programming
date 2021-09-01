cd6c6e3da0ec4495ce06c23711008b502881b561
Philimon Derib <philimonderib2019@gmail.com>
2021-09-01 19:18:47 +0300

    add

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -56,6 +56,10 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+skiplist_t *create_skiplist(int *array, size_t size);
+void init_express(skiplist_t *list, size_t size);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */
