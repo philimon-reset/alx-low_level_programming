f86ff6da322bc682b15335a1ec63681a9f04820a
Philimon Derib <philimonderib2019@gmail.com>
2021-09-01 15:03:39 +0300

    jump

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,1 +1,33 @@
 #include "search_algos.h"
+
+/**
+ * jump_list - searches for a value in an array of integers
+ * @list: a pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: int
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t a = 0, b = 0;
+	listint_t *temp = list;
+
+	if (temp == NULL)
+		return (-1);
+
+	while ((array[b] < value) && (b) < size)
+	{
+		a = b;
+		printf("Value checked array[%lu] = [%d]\n", b, array[b]);
+		b += sqrt(size);
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", a, b);
+	for (; a <= size - 1; a++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
+		if (array[a] == value)
+			return (a);
+	}
+	return (NULL);
+}
