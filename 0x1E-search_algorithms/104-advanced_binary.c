15c3a56eb2c41d9bc09f5fdcbec585d0d176c257
Philimon Derib <philimonderib2019@gmail.com>
2021-09-02 01:00:26 +0300

    advanced binary #betty tabs from spaces

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -10,38 +10,38 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-        size_t i, mid;
-        int *temp = array;
+	size_t i, mid;
+	int *temp = array;
 
-        if (array == NULL)
-                return (-1);
+	if (array == NULL)
+		return (-1);
 
-        printf("Searching in array: ");
-        for (i = 0; i < size - 1; i++)
-                printf("%d, ", temp[i]);
-        printf("%d\n", temp[i]);
-        if (size % 2 == 0)
-                mid = (size / 2) - 1;
-        else
-                mid = size / 2;
-        if (value == temp[mid])
-        {
-                if ((temp[mid + 1] == temp[mid]) && (temp[mid - 1] != temp[mid]))
-                {
-                        return (&(temp[mid]) - array);
-                }
-                else if (temp[mid - 1] == temp[mid])
-                        return (advanced_binary(temp, mid + 1, value));
-                else
-                        return (&(temp[mid]) - array);
-        }
-        else if ((value > temp[mid]) && size > 1)
-        {
-                return (advanced_binary((temp + (mid + 1)), size / 2, value));
-        }
-        else if ((value < temp[mid]) && size > 1)
-        {
-                return (advanced_binary(temp, mid, value));
-        }
-        return (-1);
+	printf("Searching in array: ");
+	for (i = 0; i < size - 1; i++)
+		printf("%d, ", temp[i]);
+	printf("%d\n", temp[i]);
+	if (size % 2 == 0)
+		mid = (size / 2) - 1;
+	else
+		mid = size / 2;
+	if (value == temp[mid])
+	{
+		if ((temp[mid + 1] == temp[mid]) && (temp[mid - 1] != temp[mid]))
+		{
+			return (&(temp[mid]) - array);
+		}
+		else if (temp[mid - 1] == temp[mid])
+			return (advanced_binary(temp, mid + 1, value));
+		else
+			return (&(temp[mid]) - array);
+	}
+	else if ((value > temp[mid]) && size > 1)
+	{
+		return (advanced_binary((temp + (mid + 1)), size / 2, value));
+	}
+	else if ((value < temp[mid]) && size > 1)
+	{
+		return (advanced_binary(temp, mid, value));
+	}
+	return (-1);
 }
