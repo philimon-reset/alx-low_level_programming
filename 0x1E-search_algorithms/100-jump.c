6592541253d7bc77263899e58bd0a5bd431e63bc
Philimon Derib <philimonderib2019@gmail.com>
2021-09-03 00:30:09 +0300

    array jump #if this work, very disapointing

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,7 +15,7 @@ int jump_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while ((array[b] < value) && (a) < size)
+	while ((array[b] < value) && (b) < size)
 	{
 		a = b;
 		printf("Value checked array[%lu] = [%d]\n", b, array[b]);
